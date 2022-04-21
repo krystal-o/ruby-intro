c15a45db14473959fde5f4be387e08dd4eb99d62
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-04-21 01:44:35 +0000

    made my first array

--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -12,6 +12,29 @@
 # If the two lists contain the same item, only show it once!
 # Lastly, display each item in the list prepended with "buy ".
 
+bens_list =["diapers","wipes","bath toys", "toilet paper"]
+brians_list = ["beer","toilet paper", "tacos"]
+# puts bens_list.inspect
+
+combined_list = bens_list + brians_list
+puts combined_list.inspect
+
+#2. sort the lists alphabetically
+sorted_list = combined_list.sort
+puts sorted_list.inspect
+
+#3 remove duplicates from the list
+unique_list = sorted_list.uniq 
+puts unique_list.inspect
+
+#4 display each item prependendent with buy
+unique_list = sorted_list.uniq 
+puts "buy #{unique_list[0]}"
+puts "buy #{unique_list[1]}"
+puts "buy #{unique_list[2]}"
+puts "buy #{unique_list[3]}"
+puts "buy #{unique_list[4]}"
+puts "buy #{unique_list[5]}"
 # HINTS
 # Learn to read the documentation!
 # https://ruby-doc.org/core-2.7.0/Array.html
