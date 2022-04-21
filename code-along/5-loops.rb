b8b30c94bec4f92932500802209d6e54b7e14a63
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-04-21 02:28:53 +0000

    tried my first loop

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,53 @@
 #   puts "tacos!"
 # end
 
+# 1. start at index 0
+index = 0
+# 2. start my loop
+loop do 
+    if index == 5
+        #3. break out of the loop at 5 times
+        break
+    end
+    puts "tacos!"
+    #4. increment the index
+    index = index +1
+end
+
+# Loop through tacos
+tacos = ["carnitas","carne asada","pollo", "pescado"]
+index = 0
+# 2. start my loop
+loop do 
+    if index == tacos.count
+        #3. break out of the loop at 5 times
+        break
+    end
+    puts "tacos!"
+    #4. increment the index
+    index = index +1
+end
 # Loop through tacos
 # tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# ANOTHER TRY
+
+# Loop through tacos
+tacos = ["carnitas","carne asada","pollo", "pescado"]
+index = 0
+# 2. start my loop
+loop do 
+    if index == tacos.count
+        #3. break out of the loop at 5 times
+        break
+    end
+
+    taco = tacos[index]
+    puts taco
+    #4. increment the index
+    index = index +1
+end
+
+for taco in tacos
+    puts taco
+end
