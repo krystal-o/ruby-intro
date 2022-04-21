e7b2636c0ab770e7453482e3f772beb9c5407dee
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-04-21 00:20:48 +0000

    tried ruby for the first time and developed coded craps

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,40 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-
-# Perform simple math with numbers
-
+puts 5
+#it's saying print whatever data that follows out. RIght now it says "5"
+puts 3
+# Perform simple math with numbers and produces results that are similar to the format that you are inputting
+# i.e. below, I am only using integers
+puts 5+3
+puts 5-3
+puts 5*3
+puts 5/3
+puts 5%3
+#to get total numbers, you must put it in the format that you want back
+puts 5.0/3.0
 # Strings
-
-# Combine strings together
-
+puts "Hello, world"
+# Combine strings together (concatenation)
+puts "Hello, " + "world"
+puts "Tacos are " + "delish!"
+puts "Tacos " *3
+#puts 3 * Tacos
+puts "Tacos" + 3.to_s
+#
 # Variables
+a = 5
+b = 3
+puts a*b 
 
-# Combine strings and variables
+food = "tacos"
+quantity = 3
+puts food + ":" +quantity.to_s 
+
+# Combine strings and variables (intropolation); the pound sign tells ruby that what is in {} is code
+puts "#{food}: #{quantity}"
+first_name = "Ronald McDonald"
+puts "Hello, #{first_name}"
 
 # String manipulation
+puts "Hello".reverse
